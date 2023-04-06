ce60515872ce5fe3757ffa6a26d513686d11d863
Express01 <115688744+Express01@users.noreply.github.com>
2023-04-06 17:27:12 +0300

    Создать классы DTO

---
+++ DeliveryService/Dtos/DeliveryReadDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeliveryService.Dtos
+{
+    public class DeliveryReadDto
+    {
+       
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
