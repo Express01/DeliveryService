041143cfffcde0d7db1a553f68ed781eea2c865b
Express01 <115688744+Express01@users.noreply.github.com>
2023-04-06 17:33:47 +0300

    Добавить AutoMapper

---
+++ DeliveryService/Profiles/DeliveriesFile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using DeliveryService.Dtos;
+using DeliveryService.Models;
+
+namespace DeliveryService.Profiles
+{
+    public class DeliveriesFile:Profile
+    {
+        public DeliveriesFile()
+        {
+            CreateMap<Delivery, DeliveryReadDto>();
+            CreateMap<DeliveryCreateDto, Delivery>();   
+        }
+    }
+}
